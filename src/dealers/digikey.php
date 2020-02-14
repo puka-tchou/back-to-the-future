9a5fbbdff39879175858df61dc1f62f5bedb3e9d
Gaspard Bariller <gaspard.bariller@tutanota.com>
2020-02-14 12:21:17 +0100

    dev: add notes about Digikey

---
+++ src/dealers/digikey.php
@@ -0,0 +1,12 @@
+<?php namespace dealers\DigiKey;
+
+class DigiKey
+{
+    /**@todo Implementation may need NodeJS because DigiKey dynamically generates its pages.
+     * @return string
+     */
+    public function getStock(): string
+    {
+        return 'Implementation may need NodeJS because DigiKey dynamically generates its pages.';
+    }
+}
