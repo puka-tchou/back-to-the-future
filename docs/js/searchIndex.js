a84dea85e4e0a278517e97dabc7d5528110338ad
Gaspard Bariller <gaspard.bariller@tutanota.com>
2020-02-18 10:15:23 +0100

    docs: generate documentation

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,96 @@
+Search.appendIndex([
+  {
+    fqsen: '\\dealers\\AlliedElec\\AlliedElec',
+    name: 'AlliedElec',
+    summary: 'AlliedElec\u0020distributor\u0020stock\u0020check.',
+    url: '../classes/dealers-AlliedElec-AlliedElec.html'
+  },
+  {
+    fqsen: '\\dealers\\AlliedElec\\AlliedElec\u003A\u003AgetStock\u0028\u0029',
+    name: 'getStock',
+    summary:
+      'Check\u0020the\u0020stock\u0020of\u0020a\u0020given\u0020part\u0020number.',
+    url: '../classes/dealers-AlliedElec-AlliedElec.html#method_getStock'
+  },
+  {
+    fqsen: '\\dealers\\DigiKey\\DigiKey',
+    name: 'DigiKey',
+    summary: '',
+    url: '../classes/dealers-DigiKey-DigiKey.html'
+  },
+  {
+    fqsen: '\\dealers\\DigiKey\\DigiKey\u003A\u003AgetStock\u0028\u0029',
+    name: 'getStock',
+    summary: 'Get\u0020stock\u0020information\u0020from\u0020DigiKey.',
+    url: '../classes/dealers-DigiKey-DigiKey.html#method_getStock'
+  },
+  {
+    fqsen: '\\Stock\\Stock',
+    name: 'Stock',
+    summary:
+      'Retrieve\u0020the\u0020current\u0020stock\u0020informations\u0020from\u0020online\u0020stores.',
+    url: '../classes/Stock-Stock.html'
+  },
+  {
+    fqsen: '\\Stock\\Stock\u003A\u003Aget\u0028\u0029',
+    name: 'get',
+    summary: '',
+    url: '../classes/Stock-Stock.html#method_get'
+  },
+  {
+    fqsen: '\\utilities\\Setlist\\Setlist',
+    name: 'Setlist',
+    summary:
+      'Construct\u0020a\u0020list\u0020of\u0020part\u0020numbers\u0020from\u0020a\u0020file.',
+    url: '../classes/utilities-Setlist-Setlist.html'
+  },
+  {
+    fqsen: '\\utilities\\Setlist\\Setlist\u003A\u003AreadFromFile\u0028\u0029',
+    name: 'readFromFile',
+    summary:
+      'Reads\u0020a\u0020file\u0020input\u0020in\u0020YAML\u0020format\u0020and\u0020output\u0020an\u0020array\u0020containing\u0020the\u0020part\u0020numbers.',
+    url: '../classes/utilities-Setlist-Setlist.html#method_readFromFile'
+  },
+  {
+    fqsen: '\\',
+    name: '\\',
+    summary: '',
+    url: '../namespaces/default.html'
+  },
+  {
+    fqsen: '\\dealers\\AlliedElec',
+    name: 'AlliedElec',
+    summary: '',
+    url: '../namespaces/dealers-alliedelec.html'
+  },
+  {
+    fqsen: '\\dealers',
+    name: 'dealers',
+    summary: '',
+    url: '../namespaces/dealers.html'
+  },
+  {
+    fqsen: '\\dealers\\DigiKey',
+    name: 'DigiKey',
+    summary: '',
+    url: '../namespaces/dealers-digikey.html'
+  },
+  {
+    fqsen: '\\Stock',
+    name: 'Stock',
+    summary: '',
+    url: '../namespaces/stock.html'
+  },
+  {
+    fqsen: '\\utilities\\Setlist',
+    name: 'Setlist',
+    summary: '',
+    url: '../namespaces/utilities-setlist.html'
+  },
+  {
+    fqsen: '\\utilities',
+    name: 'utilities',
+    summary: '',
+    url: '../namespaces/utilities.html'
+  }
+]);
