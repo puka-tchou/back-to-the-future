619fe642fcc682b86301ce551f8c0783e8027f37
Gaspard Bariller <gaspard.bariller@tutanota.com>
2020-02-20 12:56:00 +0100

    housekeeping: move product function to Product class

--- src/api/data/Database.php
+++ src/api/data/Database.php
@@ -53,27 +53,4 @@ class Database
 
         return ($res[0] == 1);
     }
-
-    /** Add a product to the database.
-     * @param string $partNumber The product part-number.
-     * @param integer $updateInterval The time, in days, between each update of the stock and the prices.
-     * @param string $manufacturer The part manufacturer.
-     *
-     * @return bool `true` if the creation succeded.
-     */
-    public function addProduct(string $partNumber, int $updateInterval, string $manufacturer): bool
-    {
-        $partNumber = strtoupper($partNumber);
-        $tracked_since = date('Y-m-d');
-        $database = new Database;
-        $query = $database->connection->prepare('INSERT INTO products (part_number, tracked_since, update_interval, state, manufacturer) VALUES (?, ?, ?, ?, ?);');
-
-        return $query->execute(array(
-            $partNumber,
-            $tracked_since,
-            $updateInterval,
-            'PENDING',
-            $manufacturer
-        ));
-    }
 }
