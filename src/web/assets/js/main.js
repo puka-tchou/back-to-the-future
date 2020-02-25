9844780c95aa69c28fd1cb06abe9cc36df9908fe
Gaspard Bariller <gaspard.bariller@tutanota.com>
2020-02-25 15:08:18 +0100

    web: improve result display

--- src/web/assets/js/main.js
+++ src/web/assets/js/main.js
@@ -16,19 +16,26 @@ const getDataFromAPI = fileInput => {
     })
     .then(json => {
       let aobj = [];
+      const target = document.getElementById('result-table');
+      target.innerText = '';
       for (const key in json) {
         if (json.hasOwnProperty(key)) {
           json[key]['part-number'] = key;
           aobj.push(json[key]);
-
           const row = document.createElement('tr');
           const partCell = row.insertCell();
           const statusCell = row.insertCell();
           const responseCell = row.insertCell();
+
           partCell.innerText = key;
-          statusCell.innerText = json[key]['err'] ? 'Error' : 'OK';
-          responseCell.innerText = json[key]['response'];
-          document.getElementById('result-table').appendChild(row);
+          const stock = JSON.parse(json[key]['stock']);
+          console.log(stock);
+          if (stock['err']) {
+            statusCell.innerText = 'Error';
+            responseCell.innerText = stock['response'];
+          } else {
+          }
+          target.appendChild(row);
         }
       }
       const sheet = XLSX.utils.json_to_sheet(aobj);
@@ -52,13 +59,15 @@ const getStockFromFile = input => {
     data = reader.result.split('\r\n').filter((value, index, self) => {
       return self.indexOf(value) === index;
     });
+    const target = document.getElementById('part-table');
+    target.innerText = '';
     data.forEach((line, index) => {
       const row = document.createElement('tr');
       const idCell = row.insertCell();
       const contentCell = row.insertCell();
       idCell.innerText = index;
       contentCell.innerText = line;
-      document.getElementById('part-table').appendChild(row);
+      target.appendChild(row);
     });
     getDataFromAPI(input);
   });
