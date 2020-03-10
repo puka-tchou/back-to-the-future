9c1d595a6e2ff8d9a69b7caa944013c5e1c5d59c
Gaspard Bariller <gaspard.bariller@tutanota.com>
2020-03-10 10:47:06 +0100

    api: add netCOMPONENTS as a source

--- src/api/dealers/NetComponents.php
+++ src/api/dealers/NetComponents.php
@@ -2,75 +2,74 @@
 
 use dealers\iDealer\iDealer;
 
-class NetComponents implements iDealer
+class NetComponents
 {
     /**
      * @return mixed[]
      */
-    public function getStock(string $part_number): array
+    public function getStock(): array
     {
-        $distributor_id = $_GET['id'];
-        $distributor_id_short = '';
-
-        define('DEFAULT_URL', 'https://api.netcomponents.com/api/DILP/v3');
+        define('BASE_URL', 'https://api.netcomponents.com/api/DILP/v3');
         define('DEFAULT_AUTH', 'Authorization: Basic ' . base64_encode('crouzet:33abd2799589'));
         define('DEFAULT_CONTENT', 'Content-Type:application/json');
-        
-        if (!isset($_SESSION['OAuth'])) {
-            do_api_login();
+        define('LOGIN', 'OAuth');
+        if (!isset($_SESSION[LOGIN])) {
+            $this->do_api_login();
+        }
+        die();
+    }
+
+    private function do_api_login(): void
+    {
+        $response = $this->get_api_response('/Login', DEFAULT_AUTH, 'POST', array(DEFAULT_CONTENT), array(), false);
+        $login_info = json_decode($response, true);
+        $_SESSION[LOGIN] = 'Authorization: '.$login_info['AuthToken'];
+        var_dump($response);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    private function get_api_response($url, $auth, $method, $headers = array(DEFAULT_CONTENT), $postData = array(), $retryOnFailure = true)
+    {
+        $process = curl_init(BASE_URL.$url);
+        $hdrs    = $headers;
+        $hdrs[]  = $auth;
+
+        curl_setopt($process, CURLOPT_HTTPHEADER, $hdrs);
+        curl_setopt($process, CURLOPT_TIMEOUT, 30);
+    
+        curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($process, CURLOPT_VERBOSE, 1);
+        curl_setopt($process, CURLOPT_HEADER, 1);
+    
+        if ($method === 'POST') {
+            curl_setopt($process, CURLOPT_POST, 1);
+            curl_setopt($process, CURLOPT_POSTFIELDS, $postData);
+        } else {
+            curl_setopt($process, CURLOPT_POST, false);
         }
 
-        function do_api_login()
-        {
-            $return = get_api_response('/Login?TTL=900', 'POST', array(DEFAULT_CONTENT), DEFAULT_AUTH, array(), false); // do not retry if failed
-            $login_info = json_decode($return, true);
-            $_SESSION['OAuth'] = 'Authorization: '.$login_info['AuthToken'];
-        }
+        $response = curl_exec($process);
 
-        function get_api_response($url, $method = 'POST', $headers = array(DEFAULT_CONTENT), $auth, $postData = array(), $retryAfterLogin = true)
-        {
-            $process = curl_init(DEFAULT_URL.$url);
-            $hdrs    = $headers;
-            $hdrs[]  = $auth;
+        $http_status = curl_getinfo($process, CURLINFO_HTTP_CODE);
+        $error = curl_error($process);
+        $header_size = curl_getinfo($process, CURLINFO_HEADER_SIZE);
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
 
-            curl_setopt($process, CURLOPT_HTTPHEADER, $hdrs);
-            curl_setopt($process, CURLOPT_TIMEOUT, 30);
-    
-            curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($process, CURLOPT_VERBOSE, 1);
-            curl_setopt($process, CURLOPT_HEADER, 1);
-    
-            if ($method == 'POST') {
-                curl_setopt($process, CURLOPT_POST, 1);
-                curl_setopt($process, CURLOPT_POSTFIELDS, $postData);
+        curl_close($process);
+
+        // if doesn't return OK header, login and retry
+        if ($http_status != 200) {
+            if ($http_status == 401 && $retryOnFailure) {
+                do_api_login();
+                return get_api_response($url, $method, $headers, $_SESSION[LOGIN], $postData, false);
             } else {
-                curl_setopt($process, CURLOPT_POST, false);
+                die($header . '<br />' . $error);
             }
-            curl_setopt($process, CURLOPT_RETURNTRANSFER, true);
-
-            $return = curl_exec($process);
-
-            $http_status = curl_getinfo($process, CURLINFO_HTTP_CODE);
-            $error = curl_error($process);
-            $header_size = curl_getinfo($process, CURLINFO_HEADER_SIZE);
-            $header = substr($return, 0, $header_size);
-            $body = substr($return, $header_size);
-
-            curl_close($process);
-
-            // if doesn't return OK header, login and retry
-            if ($http_status != 200) {
-                if ($http_status == 401 && $retryAfterLogin) {
-                    do_api_login();
-                    return get_api_response($url, $method, $headers, $_SESSION['OAuth'], $postData, false);
-                } else {
-                    die($header . '<br />' . $error);
-                }
-            }
-
-            return $body;
         }
 
-        return array();
+        return $body;
     }
 }
