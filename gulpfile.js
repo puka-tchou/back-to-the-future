83aa7e155b657dc618c532f393ffc88980557e1e
Gaspard Bariller <gaspard.bariller@tutanota.com>
2020-03-12 15:59:06 +0100

    build: optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
 const { dest, src, parallel } = require('gulp');
 const babel = require('gulp-babel');
 const htmlmin = require('gulp-htmlmin');
+const image = require('gulp-image');
 const postcss = require('gulp-postcss');
 
 function api() {
   return src('src/api/**/*').pipe(dest('dist/api/'));
-}
-
-function assets() {
-  return src('src/web/assets/images/**/*').pipe(dest('dist/web/assets/images'));
 }
 
 function css() {
@@ -29,10 +26,16 @@ function html() {
     .pipe(dest('dist/web/'));
 }
 
+function images() {
+  return src('src/web/**/*.{jpg,jpeg,png,gif,svg}')
+    .pipe(image())
+    .pipe(dest('dist/web'));
+}
+
 function js() {
   return src('src/web/assets/js/*.js')
     .pipe(babel())
     .pipe(dest('dist/web/assets/js'));
 }
 
-exports.build = parallel(api, assets, css, html, js);
+exports.build = parallel(api, images, css, html, js);
