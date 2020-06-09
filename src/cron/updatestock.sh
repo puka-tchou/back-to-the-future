9796f4a1efef725a339d950dfa1ab277fb156f00
Gaspacchio <gaspard.bariller@tutanota.com>
2020-06-09 17:24:33 +0200

    cron: update the cron task

--- src/cron/updatestock.sh
+++ src/cron/updatestock.sh
@@ -1,4 +1,4 @@
 # cron task to update the stock of all the parts each week
 #
 # This will launch the task each monday at 00:00
-# 0 0 * * MON
+# 0 0 * * MON /usr/bin/php /opt/api/tasks/UpdateStock.php
