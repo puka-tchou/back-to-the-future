9aaf0663c8fbf93158c13de5ec063b81839a0d73
Gaspacchio <gaspard.bariller@tutanota.com>
2020-07-03 13:18:46 +0200

    feat(web): improve the table display of the results

--- src/web/assets/js/create-table.js
+++ src/web/assets/js/create-table.js
@@ -3,53 +3,37 @@ import 'gridjs/dist/theme/mermaid.css';
 import { clearActiveState } from './clear-active-state';
 
 export const createTable = (body) => {
-  const resultTable = document.querySelector('#result-table');
-  const resultInfo = document.querySelector('#result-table-info');
-  const fragment = document.createDocumentFragment();
   const statusInfo = document.querySelector('#status-info');
-
-  /*
-  document.querySelector('#data-result').innerText = '';
-  const grid = new Grid({
-    columns: ['Name', 'Email', 'Phone Number'],
-    data: [
-      ['John', 'john@example.com', '(353) 01 222 3333'],
-      ['Mark', 'mark@gmail.com', '(01) 22 888 4444'],
-    ],
-  });
-  grid.render(document.querySelector('#data-result'));
-  */
-
-  let i = 0;
-  performance.mark('table-start');
-
-  resultTable.textContent = '';
+  const data = [];
 
   console.log('📋 table creation has started.');
+  performance.mark('table-start');
+
   for (const part in body) {
     if (Object.prototype.hasOwnProperty.call(body, part)) {
-      const row = document.createElement('tr');
-      const partCell = row.insertCell();
-      const statusCell = row.insertCell();
-      const responseCell = row.insertCell();
       const stock = body[part].body;
+      let totalStock = 0;
+      let numberOfSuppliers = 0;
 
       stock.forEach((record) => {
-        if (record.state === '0' && record.parts_in_stock !== '-1') {
-          i++;
-          partCell.textContent = record.part_number;
-          statusCell.textContent =
-            record.state === '0' ? i + ' records found.' : '';
-          responseCell.textContent += record.parts_in_stock;
-          responseCell.textContent += ', ';
-          fragment.append(row);
-        }
+        totalStock += Number(record.parts_in_stock);
+        numberOfSuppliers++;
       });
+
+      data.push([part, totalStock, numberOfSuppliers]);
     }
   }
 
-  resultTable.append(fragment);
-  resultInfo.textContent = `${i} stock records found.`;
+  const grid = new Grid({
+    columns: ['Part number', 'Total stock', 'Number of different suppliers'],
+    data,
+    sort: true,
+    pagination: {
+      enabled: true,
+      limit: 50,
+    },
+  });
+  grid.render(document.querySelector('#data-result'));
 
   clearActiveState();
   console.log(`⭕ table creation ended. ${i} stock records found.`);
