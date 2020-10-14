6b0abe630d4f4d11b2e07c4d2a1ae441d6942f83
Gaspacchio <gaspard.bariller@tutanota.com>
2020-10-14 15:49:26 +0200

    tests: start to work on Route() testing

---
+++ tests/RouteTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use route\Route\Route;
+
+class RouteTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testAddSendsDefaultIfFileIsNull()
+    {
+        $route = new Route();
+        $code = 2;
+        $shortMessage = 'The CSV file was not found.';
+        $body[] =  'The CSV file containing the part-numbers was not found in your request. Please, make sure that you are sending a "multipart/form-data" request with a "parts" field containing the CSV file. The "parts" field should be of type "file".';
+        $expected = array(
+      'code' => $code,
+      'message' => $shortMessage,
+      'body'=> $body
+    );
+
+        $this->expectOutputString(json_encode($expected));
+        $route->add();
+    }
+}
