72c38486f4a5153d55d5fe0b48ff62540e5cc45c
Gaspacchio <gaspard.bariller@tutanota.com>
2020-11-26 10:48:19 +0100

    tests: configure mocha and istanbul

--- tests/helpersTest.js
+++ tests/helpersTest.js
@@ -1,11 +1,12 @@
+import chai from 'chai';
 import { containsForbiddenChar } from '../src/web/js/helpers.js';
 
 describe('containsForbiddenChar()', () => {
 	it('should return true if the string is valid', () => {
-		expect(containsForbiddenChar('a')).toBe(false);
+		chai.expect(containsForbiddenChar('a')).to.be.false;
 	});
 
 	it('should return false if there is a forbidden character', () => {
-		expect(containsForbiddenChar('*')).toBe(true);
+		chai.expect(containsForbiddenChar('*')).to.be.true;
 	});
 });
